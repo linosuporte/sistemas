c4db5d94e9de09c2fec4b1677c7997378fabbd57
Wendell Luiz Ferreira Lino <wflino@stefanini.com>
2020-03-31 22:38:46 -0300

    Primeiro commit.

---
+++ projeto-rest/src/test/java/apirest/ProjetoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
